ef1f3cbe6bb1de0a170f12b89bbb840938e4f035
Alexandra <aleruoj@gmail.com>
2019-12-10 18:12:39 -0500

    ajuste min-height tutores y copy calendar

--- js/custom.js
+++ js/custom.js
@@ -2450,31 +2450,30 @@ if (typeof NProgress != 'undefined') {
 				  },
 				  editable: true,
 				  events: [{
-					title: 'All Day Event',
+					title: 'Matemáticas',
 					start: new Date(y, m, 1)
 				  }, {
-					title: 'Long Event',
+					title: 'Idiomas: Inglés',
 					start: new Date(y, m, d - 5),
 					end: new Date(y, m, d - 2)
 				  }, {
-					title: 'Meeting',
+					title: 'Home schooling',
 					start: new Date(y, m, d, 10, 30),
 					allDay: false
 				  }, {
-					title: 'Lunch',
+					title: 'Química',
 					start: new Date(y, m, d + 14, 12, 0),
 					end: new Date(y, m, d, 14, 0),
 					allDay: false
 				  }, {
-					title: 'Birthday Party',
+					title: 'Álgebra',
 					start: new Date(y, m, d + 1, 19, 0),
 					end: new Date(y, m, d + 1, 22, 30),
 					allDay: false
 				  }, {
-					title: 'Click for Google',
+					title: 'Idiomas: Español',
 					start: new Date(y, m, 28),
 					end: new Date(y, m, 29),
-					url: 'http://google.com/'
 				  }]
 				});
 				
