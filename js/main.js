dfcba8d2151bbf66212f5f0408f26a524324ebaa
aleruoj <aleruoj@gmail.com>
2020-03-31 11:51:54 -0500

    Añadimos timepicker, creacion de main js y actualizacion de url

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//AM-PM TIME DE CREAR EVENTO CLASE
+$('#hora-inicio').timepicker();
+// Fecha del día 
+$(document).ready(function () {
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    var today = year + "-" + month + "-" + day;
+    $("#fecha").attr("value", today);
+});
